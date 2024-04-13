eaa5b9a3c4055be6c97aa94080772ce536d7f13b
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-13 12:45:11 -0400

    update img in coursesData

--- src/data/CoursesData.js
+++ src/data/CoursesData.js
@@ -1,7 +1,7 @@
 let coursesData = [
-          { id: 1, title: "20 Libs for js devs", price: 290_000, img: '/images/20libs.png' },
-          { id: 2, title: "React Course", price: 2_000_000, img: '/images/react.png' },
-          { id: 3, title: "Npm For Js Devs", price: "Free", img: '/images/npm.png' },
+          { id: 1, title: "20 Libs for js devs", price: 290_000, img: './../../Images/20libs.png' },
+          { id: 2, title: "React Course", price: 2_000_000, img: './../../Images/react.png' },
+          { id: 3, title: "Npm For Js Devs", price: "Free", img: './../../Images/npm.png' },
 ]
 
 export default coursesData
