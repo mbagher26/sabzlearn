c9959bc9b93b76f3f935e48c8665c237b90fc753
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-13 10:04:42 -0400

    Integrate react-router-dom into App component for routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Header from './components/header/Header';
-
+import { Routes ,Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Courses from './components/courses/Courses';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/courses' element={<Courses/>} />
+      </Routes>
     </div>
   );
 }
