6829d20157e793886f3bb50ee539799ce6cca5ef
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-13 07:00:37 -0400

    Refactor Header component to use React Bootstrap Navbar and react-router-dom Links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+          return (
+                    <>
+                              <Container>
+                                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                                        <Nav className="me-auto">
+                                                  <Link to="/home">Home</Link>
+                                                  <Link to="/courses">sourses</Link>
+                                                  <Link to="/about">about</Link>
+                                        </Nav>
+                              </Container>
+                    </>
+          )
 }
 
 export default Header
