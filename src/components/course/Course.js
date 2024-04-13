f7dc29d0e133ddc0a2bd715515fa0cf30b8b5e3a
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-13 12:41:31 -0400

    Update Course component to include Bootstrap Card and Link to main course

--- src/components/course/Course.js
+++ src/components/course/Course.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
-function Course() {
+
+function Course(props) {
+
   return (
-    <div>Course</div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={props.img} />
+      <Card.Body>
+        <Card.Title>{props.title }</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Link to={`/course/${props.id}`}>Go somewhere</Link>
+      </Card.Body>
+    </Card>
   )
 }
 
