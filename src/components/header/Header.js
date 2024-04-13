a5b7de77f2ec8dbfc74fa367620dd782f085dd6b
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-13 09:33:05 -0400

    Update Header component to use Bootstrap Navbar with a primary background and dark theme

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,14 +7,16 @@ import { Link } from 'react-router-dom';
 function Header() {
           return (
                     <>
-                              <Container>
-                                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                                        <Nav className="me-auto">
-                                                  <Link to="/home">Home</Link>
-                                                  <Link to="/courses">sourses</Link>
-                                                  <Link to="/about">about</Link>
-                                        </Nav>
-                              </Container>
+                              <Navbar bg="primary" data-bs-theme="dark">
+                                        <Container>
+                                                  <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                                                  <Nav className="me-auto">
+                                                            <Link to="/home">Home</Link>
+                                                            <Link to="/courses">sourses</Link>
+                                                            <Link to="/about">about</Link>
+                                                  </Nav>
+                                        </Container>
+                              </Navbar>
                     </>
           )
 }
