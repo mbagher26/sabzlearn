b89cb249ecf923d66c724c0ae701d5283d14b5bb
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-13 12:56:08 -0400

    Update Courses component to include CSS styling

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import coursesData from '../../data/CoursesData'
 import Course from '../course/Course'
+import './../../styles/Courses.css'
 
 function Courses() {
   return (
-    <>
+    <div className='courses-container'>
       {
         coursesData.map(course => (
           <Course key={course.id} {...course} />
         ))
       }
-    </>
+    </div>
   )
 }
 
