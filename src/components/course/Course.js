7d0ec98867a2945184ce183172f8ea4b311f1e8b
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-13 14:26:43 -0400

    Update Course component to include Link wrapping around Card and price display in card

--- src/components/course/Course.js
+++ src/components/course/Course.js
@@ -6,17 +6,18 @@ import { Link } from 'react-router-dom';
 function Course(props) {
 
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={props.img} />
-      <Card.Body>
-        <Card.Title>{props.title }</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Link to={`/course/${props.id}`}>Go somewhere</Link>
-      </Card.Body>
-    </Card>
+    <Link to={`/course/${props.id}`}>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={props.img} style={{width:'100%', height: '150px'}} />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>
+            {props.price}
+          </Card.Text>
+          <Link to={`/course/${props.id}`}>Go somewhere</Link>
+        </Card.Body>
+      </Card>
+    </Link>
   )
 }
 
