d339960d1acb73850f698bf8c63c423d2cf91413
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-14 08:37:24 -0400

    Update About component to include navigation links and nested routes

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link , Outlet} from 'react-router-dom'
 
 function About() {
   return (
-    <div>About</div>
+    <div style={{textAlign:'center'}}>
+      <h3>Wellcome To About Page )):</h3>
+      <div style={{display:'flex', justifyContent:'space-evenly'}}>
+        <Link to='setting'>setting</Link>
+        <Link to='dashbord'>dashbord</Link>
+      </div>
+    <Outlet/>
+    </div>
   )
 }
 
