e19f2ef27b517e40b95d55205add3d2422c9fd3d
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-14 08:48:10 -0400

    Update Course component to use local image and include CSS styling

--- src/components/course/Course.js
+++ src/components/course/Course.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
-
-
+import img1 from './../../Images/20libs.png'
+import './../../styles/course.css'
 function Course(props) {
 
   return (
-    <Link to={`/course/${props.id}`}>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={props.img} style={{width:'100%', height: '150px'}} />
-        <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-          <Card.Text>
-            {props.price}
-          </Card.Text>
-          <Link to={`/course/${props.id}`}>Go somewhere</Link>
-        </Card.Body>
-      </Card>
-    </Link>
+    <div className='course-container'>
+      <Link to={`/course/${props.id}`}>
+        <Card style={{ width: '16rem' }}>
+          <Card.Img variant="top" src={img1} style={{ width: '100%', height: '150px' }} />
+          <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            <Card.Text>
+              {props.price}
+            </Card.Text>
+            <Link to={`/course/${props.id}`}>Go somewhere</Link>
+          </Card.Body>
+        </Card>
+      </Link>
+    </div>
   )
 }
 
