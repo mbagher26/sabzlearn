7f97e2a33b2b23760c571572a5554782dbed25de
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-14 13:52:09 -0400

    Expand routes to include Login, Panel, Dashbord components and PrivateRoute logic

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,10 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Courses from './components/courses/Courses';
 import Maincourse from './components/maincourse/Maincourse';
+import Login from './components/login/Login';
+import Panel from './components/panel/Panel';
+import Dashbord from './components/dashbord/Dashbord';
+import PrivateRoute from './components/privateroute/PrivateRoute';
 
 
 let routes = [
@@ -16,10 +20,15 @@ let routes = [
                               { path: 'dashbord', element: <h1>Dashbord</h1> }
 
                     ]
+          },
+          { path: '/login', element: <Login /> },
+          {
+                    path: '/*', element: <PrivateRoute />, children: [
+          
+                              { path: 'panel', element: <Panel /> },
+                              { path: 'dashbord', element: <Dashbord />}
+                    ]
           }
-
-
-
 ]
 
 export default routes
