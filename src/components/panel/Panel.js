905487148be07c3339bb10b8c2e2f5794a7b38df
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-14 13:07:12 -0400

    create Panel component

---
+++ src/components/panel/Panel.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Panel() {
+
+          return (
+
+
+
+                    <div style={{textAlign:'center'}}> Panel</div >
+
+
+          )
+}
+
+export default Panel
