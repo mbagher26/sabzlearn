f72c5e07ea979b5cf49b421c1da8461a3b1d96c7
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-14 08:55:11 -0400

    Refactor Courses component to use React Bootstrap Grid system

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -2,15 +2,21 @@ import React from 'react'
 import coursesData from '../../data/CoursesData'
 import Course from '../course/Course'
 import './../../styles/Courses.css'
+import { Col, Row } from 'react-bootstrap';
+
 
 function Courses() {
   return (
     <div className='courses-container'>
-      {
-        coursesData.map(course => (
-          <Course key={course.id} {...course} />
-        ))
-      }
+      <Row>
+        {
+          coursesData.map(course => (
+            <Col sm={6} md={4}>
+              <Course key={course.id} {...course} />
+            </Col>
+          ))
+        }
+      </Row>
     </div>
   )
 }
