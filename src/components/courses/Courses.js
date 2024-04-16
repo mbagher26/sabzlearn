c074090cf6d810dad41bc416c4023de5905955d4
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-16 09:22:00 -0400

    Add useLocation hook to Courses component to access location information

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -3,9 +3,12 @@ import coursesData from '../../data/CoursesData'
 import Course from '../course/Course'
 import './../../styles/Courses.css'
 import { Col, Row } from 'react-bootstrap';
-
+import { useLocation } from 'react-router-dom';
 
 function Courses() {
+
+  let location = useLocation();
+  console.log(location);
   return (
     <div className='courses-container'>
       <Row>
