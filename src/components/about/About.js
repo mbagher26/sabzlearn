a32b4acbdae9837c070a20156aff4f72d5e185b7
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-16 09:31:39 -0400

    Add navigation to login page in About component

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Link , Outlet} from 'react-router-dom'
+import { Link , Outlet, useNavigate} from 'react-router-dom'
+
 
 function About() {
+  let navigate = useNavigate()
+
   return (
-    <div style={{textAlign:'center'}}>
+    <div style={{ textAlign: 'center' }}>
+      <button onClick={
+        () => navigate('/login', {replace: false})
+      }>ثبت نام</button>
       <h3>Wellcome To About Page )):</h3>
       <div style={{display:'flex', justifyContent:'space-evenly'}}>
         <Link to='setting'>setting</Link>
