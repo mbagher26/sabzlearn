732cc6823593f8d026848c0c2e0aa3c3b3ecf64c
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-16 09:18:46 -0400

    Update Header component to use NavLink instead of Link, and add styles for active links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,21 +2,22 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './../../styles/Header.css'
 
 function Header() {
           return (
                     <>
                               <Navbar bg="primary" data-bs-theme="dark">
                                         <Container>
-                                                  <Link to="/home" style={{color:'white'}}>Navbar</Link>
+                                                  <NavLink to="/home" style={{color:'white'}}>Navbar</NavLink>
                                                   <Nav className="me-auto">
-                                                            <Link to="/home" style={{marginLeft: '35px', color:'white'}}>Home</Link>
-                                                            <Link to="/courses" style={{marginLeft: '35px', color:'white'}}>Courses</Link>
-                                                            <Link to="/about" style={{ marginLeft: '35px', color: 'white' }}>About</Link>
-                                                            <Link to="/login" style={{ marginLeft: '35px', color: 'white' }}>Login</Link>
-                                                            <Link to="/panel" style={{ marginLeft: '35px', color: 'white' }}>Panel</Link>
-                                                            <Link to="/dashbord" style={{ marginLeft: '35px', color: 'white' }}>Dashbord</Link>
+                                                            <NavLink to="/home" style={{marginLeft: '35px', color:'white'}}>Home</NavLink>
+                                                            <NavLink className={(link) => link.isActive ? 'active' : '' } to="/courses" style={{marginLeft: '35px', color:'white'}}>Courses</NavLink>
+                                                            <NavLink to="/about" style={{ marginLeft: '35px', color: 'white' }}>About</NavLink>
+                                                            <NavLink to="/login" style={{ marginLeft: '35px', color: 'white' }}>Login</NavLink>
+                                                            <NavLink to="/panel" style={{ marginLeft: '35px', color: 'white' }}>Panel</NavLink>
+                                                            <NavLink to="/dashbord" style={{ marginLeft: '35px', color: 'white' }}>Dashbord</NavLink>
 
                                                   </Nav>
                                         </Container>
