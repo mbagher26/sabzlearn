6d5175034432d6c10fca3fa2b4d97b4ddbceee66
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-16 09:28:56 -0400

    Update Course component to use img prop for image source

--- src/components/course/Course.js
+++ src/components/course/Course.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
-import img1 from './../../Images/20libs.png'
 import './../../styles/course.css'
+
 function Course(props) {
 
   return (
     <div className='course-container'>
       <Link to={`/course/${props.id}`}>
         <Card style={{ width: '16rem' }}>
-          <Card.Img variant="top" src={img1} style={{ width: '100%', height: '150px' }} />
+          <Card.Img variant="top" src={props.img} style={{ width: '100%', height: '150px' }} />
           <Card.Body>
             <Card.Title>{props.title}</Card.Title>
             <Card.Text>
